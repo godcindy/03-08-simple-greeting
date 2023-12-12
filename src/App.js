87de19e83ecc76ceb6b01271a577e028bd895e95
godcindy <cindygodoy@pursuit.org>
2023-12-11 22:40:14 -0500

    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SimpleGreeting from './SimpleGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1><SimpleGreeting /></h1>
+    </div>
+  );
+}
+
+export default App; 
---
+++ src/SimpleGreeting.js
@@ -0,0 +1,6 @@
+
+
+function SimpleGreeting() {
+    return <h1>Hello, Welcome to React!</h1>;
+  }
+  export default SimpleGreeting
